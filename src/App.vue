7e5b72ae1ea3e952a255949cc3fe771a2f298c0a
feiyangss <791496086@qq.com>
2022-02-20 17:50:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+   <h2>我是双城</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+ }
+</script>
+
+<style scoped>
+
+</style>
